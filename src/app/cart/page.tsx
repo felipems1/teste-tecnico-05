9fffd69533c0f65ab98009a9d67966f0c35b8509
felipe <083felype@gmail.com>
2024-06-20 16:32:03 -0300

    feat: add cart page

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,12 @@
+import { CartProducts } from './components/cart-products'
+import { Header } from './components/header'
+
+export default function Cart() {
+  return (
+    <div>
+      <Header />
+
+      <CartProducts />
+    </div>
+  )
+}
